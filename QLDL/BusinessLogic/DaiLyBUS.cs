70bc9645e68df3faac2006394eece5c2c4509923
vominhquan <95.vominhquan@gmail.com>
2017-03-05 19:07:27 +0700

    entity framework + 3 Layers model + DaiLy form

---
+++ QLDL/BusinessLogic/DaiLyBUS.cs
@@ -0,0 +1,71 @@
+﻿using QLDL.DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QLDL.BusinessLogic
+{
+
+    class DaiLyBUS
+    {
+        public ObservableCollection<vwDAILY_LOAIDL_QUAN> getAllDaiLy()
+        {
+            ObservableCollection<vwDAILY_LOAIDL_QUAN> allDL = new ObservableCollection<vwDAILY_LOAIDL_QUAN>();
+            using (QLDLEntities context = new QLDLEntities())
+            {
+                var alldaily = from vw in context.vwDAILY_LOAIDL_QUAN
+                               select vw;
+                foreach(var item in alldaily)
+                    allDL.Add(item);
+            }
+            return allDL;
+        }
+
+        public ObservableCollection<LOAIDL> getAllLoaiDL()
+        {
+            ObservableCollection<LOAIDL> allLoai = new ObservableCollection<LOAIDL>();
+            using (QLDLEntities context = new QLDLEntities())
+            {
+                var all = from loai in context.LOAIDLs
+                          select loai;
+                foreach (var item in all)
+                    allLoai.Add(item);
+            }
+            return allLoai;
+        }
+
+        public ObservableCollection<QUAN> getAllQuan()
+        {
+            ObservableCollection<QUAN> allQuan = new ObservableCollection<QUAN>();
+            using (QLDLEntities context = new QLDLEntities())
+            {
+                var all = from quan in context.QUANs
+                          select quan;
+                foreach (var item in all)
+                    allQuan.Add(item);
+            }
+            return allQuan;
+        }
+
+        public bool insertDaiLy(DAILY dl)
+        {
+            //try
+            //{
+                using (QLDLEntities context = new QLDLEntities())
+                {
+                    context.DAILies.Add(dl);
+                    context.SaveChanges();
+                    return true;
+                }
+            //}
+            //catch
+            //{
+            //    return false;
+            //}
+        }
+        
+    }
+}
---
+++ QLDL/Presentation/DaiLy.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using QLDL.BusinessLogic;
+using QLDL.DataAccess;
+using System.Collections.ObjectModel;
+
+namespace QLDL.Presentation
+{
+    /// <summary>
+    /// Interaction logic for DaiLy.xaml
+    /// </summary>
+    /// 
+
+    public partial class DaiLy : Window
+    {
+        private DaiLyBUS dlbus = new DaiLyBUS();
+
+        public DaiLy()
+        {
+            InitializeComponent();
+
+            // Lấy dữ liệu ban đầu
+            lsvDL.ItemsSource = dlbus.getAllDaiLy();
+            cbbLoaiDL.ItemsSource = dlbus.getAllLoaiDL();
+            cbbLoaiDL.DisplayMemberPath = "TENLOAI";
+            cbbLoaiDL.SelectedValuePath = "MALOAI";
+            cbbQuan.ItemsSource = dlbus.getAllQuan();
+            cbbQuan.DisplayMemberPath = "TENQUAN";
+            cbbQuan.SelectedValuePath = "MAQUAN";
+            
+        }
+
+        private void AddDL(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(cbbQuan.SelectedValue.ToString());
+            DAILY dl = new DAILY
+            {
+                MADL=2,
+                TENDL = txtTenDL.Text,
+                DIACHI = txtDiaChi.Text,
+                DIENTHOAI = txtDienThoai.Text,
+                NGAYTIEPNHAN = dpNgayTiepNhan.SelectedDate,
+                MAQUAN = Int32.Parse(cbbQuan.SelectedValue.ToString()),
+                LOAIDL = Int32.Parse(cbbLoaiDL.SelectedValue.ToString()),
+                TINHTRANG = 1
+            };
+            if (dlbus.insertDaiLy(dl))
+                MessageBox.Show("Đã thêm thành công");
+            else
+                MessageBox.Show("Có lỗi xảy ra");
+        }
+    }
+}
