b8ddb68392ac7bac82992905e425d57d98e53819
Phạm Hồng Phúc <phamhongphuc.info@gmail.com>
2017-05-20 14:24:32 +0700

    Gộp tất cả vào 1 cửa sổ, thêm Class DPI tác dụng chống nhòe

---
+++ QLDL/Config/DPI.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+
+namespace QLDL.Config
+{
+    class DPI
+    {
+        public static void Initialize(object sender, RoutedEventArgs e)
+        {
+            PresentationSource PresentationSource = PresentationSource.FromVisual(Application.Current.MainWindow);
+            Matrix DpiFactorMatrix = PresentationSource.CompositionTarget.TransformToDevice;
+
+            double DpiWidthFactor = DpiFactorMatrix.M11;
+            double DpiHeightFactor = DpiFactorMatrix.M22;
+
+            double scaleX = 1 / DpiWidthFactor;
+            double scaleY = 1 / DpiHeightFactor;
+
+            ((QLDL.Presentation.Main)Application.Current.MainWindow).Main.LayoutTransform = new ScaleTransform(scaleX, scaleY);
+        }
+    }
+}
